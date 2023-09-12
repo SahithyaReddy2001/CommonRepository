cf14fa8dadc94117e1ee3a7dcb2dbebb6d793678
SahithyaReddy2001 <sahithyavadiyala@gmail.com>
2023-09-12 16:14:21 +0530

    first commit

---
+++ CRUDoperations/src/test/java/Demo/CRUDoperations/CruDoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo.CRUDoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
